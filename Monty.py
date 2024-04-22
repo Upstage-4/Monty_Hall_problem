b4716ee9f72227a272227c7e64818f80b6e2a951
ktbaek72 <ktbaek72@gmail.com>
2024-04-22 18:35:20 +0900

    feat: Add function montyhall
    반복할 횟수를 입력받는 기능추가
    원하는 횟수만큼 선택하고 선택한 것에 대해 바꾸지 않았을 때의 확률과
    횟수를 출력함.
    
    resolve #2

--- Monty.py
+++ Monty.py
@@ -1,1 +1,30 @@
-print('result')
+from random import randint
+
+n = int(input("반복횟수 입력 : "))
+
+def montyhall(n):
+    normal_win_cnt = 0
+    normal_lose_cnt = 0
+    change_win_cnt = 0
+    change_lose_cnt = 0
+    
+    for i in range(n):
+        reward = randint(1, 3)
+        answer = randint(1, 3)
+        
+        val = randint(1, 3)
+        
+        while(reward == val or answer == val):
+            val = randint(1, 3)
+
+        if(reward == answer) :
+            normal_win_cnt += 1
+        else :
+            normal_lose_cnt += 1
+        
+    print(f'normal_win_cnt : {normal_win_cnt}')
+    print(f'normal_lose_cnt : {normal_lose_cnt}')
+    normal_rate = normal_win_cnt*100/(normal_win_cnt + normal_lose_cnt)
+    print(f'normal rate : {normal_rate}%')
+
+montyhall(n)
